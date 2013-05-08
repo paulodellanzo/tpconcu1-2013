9fbecd009dc651459ff4c66c4cc9182ac71b0173
paulodellanzo@gmail.com <paulodellanzo@gmail.com@f0909f3f-435a-ee1f-0906-bee0295c6d7e>
2013-05-08 02:50:41 +0000

    Agregado el que mira el puntaje

--- tpConcurrentes1/src/Observador.cc
+++ tpConcurrentes1/src/Observador.cc
@@ -14,13 +14,30 @@ int main () {
 	LockFile* tablaPuntaje = new LockFile((char *)TABLA);
 	tablaPuntaje->tomarLock();
 
-	char buffer[100];
+	FILE *archivo;
 
+	char caracteres[200];
+
+	archivo = fopen(TABLA,"r");
+
+	if (archivo == NULL)
+			exit(1);
+
+	while (feof(archivo) == 0)
+	{
+			fgets(caracteres,100,archivo);
+			printf("%s",caracteres);
+	}
+
+	fclose(archivo);
+
+	/*char buffer[100];
 	int fileDes = open ( TABLA , O_RDONLY );
 	int buffSize = 100;
 	read ( fileDes,(void *)buffer,buffSize );
 	cout << buffer;
 	close ( fileDes );
+	*/
 
 	tablaPuntaje->liberarLock();
 
